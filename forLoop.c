d28e821eb0b926f99f198219f2635f8a736c4f23
Vishnu Prasad <vishnu337247@gmail.com>
2021-03-09 12:20:39 +0530

    For loop

---
+++ forLoop.c
@@ -0,0 +1,15 @@
+/* 
+pre increment - ++a => a=a+1
+post increment - a++ => a=a++
+*/ 
+#include<stdio.h>
+
+int main(){
+    int i;
+    
+    for ( i = 0; i <= 10; i++)
+    {
+        printf("%d \n",i);
+    }
+    
+}
